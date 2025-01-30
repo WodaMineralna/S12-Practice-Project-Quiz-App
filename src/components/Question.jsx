4810b78f25f9e5eeec7f62f35876301179843acf
WodaMineralna <raknieborak65@gmail.com>
2025-01-30 20:20:15 +0100

    minor tweaks

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -5,6 +5,8 @@ import { QuizContext } from "./QuizContextProvider";
 
 const TIMER_MS = 15000;
 
+// TODO fix .css zwiazanych z last-try etc
+
 export default function Questions({ question }) {
   const { submitAnswer, setSelectedAnswer, setLastTry, answersState } =
     useContext(QuizContext);
@@ -12,7 +14,7 @@ export default function Questions({ question }) {
   useEffect(() => {
     const timeout_lastTry = setTimeout(() => {
       setLastTry()
-    }, TIMER_MS * 0.33)
+    }, TIMER_MS * 0.66)
 
     const timeout = setTimeout(() => {
       submitAnswer(question.id);

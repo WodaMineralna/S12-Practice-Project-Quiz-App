fc8d12f3281ac6f855a34a433c9f85f9eda91a18
WodaMineralna <raknieborak65@gmail.com>
2025-02-05 16:18:34 +0100

    ProgressBar.jsx code cleanup & update/upgrade

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,27 +1,22 @@
 import { useState, useEffect } from "react";
 
+const INTERVAL_MS = 10;
+
 export default function ProgressBar({ timer_ms }) {
   const [remainingTime, setRemainingTime] = useState(timer_ms);
 
-  // !
-  // * jak stworze funkcjonalnosc dismountowania componentu to nie bede musial miec tego ifa z timer_ms < 10
-  // * oraz bede mogl miec puste dependencies
   useEffect(() => {
     const interval = setInterval(() => {
-      console.log("INTERVAL - 1OMS");
+      console.log("ProgressBar INTERVAL - 1OMS");
       setRemainingTime((prevTime) => {
-        if (prevTime <= 10) {
-          clearInterval(interval);
-          return 0;
-        }
-        return prevTime - 10
+        return prevTime - INTERVAL_MS;
       });
-    }, 10);
+    }, INTERVAL_MS);
 
     return () => {
       clearInterval(interval);
     };
-  }, [timer_ms]);
+  }, []);
 
   return <progress value={remainingTime} max={timer_ms} />;
 }

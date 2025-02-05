39aa5ba64e15fbeddd4c506ac30df3a159034b2b
WodaMineralna <raknieborak65@gmail.com>
2025-02-05 17:39:58 +0100

    minor Header.jsx tweaks

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
-import pic from "/quiz-logo.png";
+import headerPic from "/quiz-logo.png";
 
 export default function Header() {
   return (
     <header>
-      <img src={pic} alt="pic" />
+      <img src={headerPic} alt="Header picture showing a quiz logo" />
       <h1>ReactQuiz</h1>
     </header>
   );
